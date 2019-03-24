50298a5cacaa8f151d942d9c636c70fbe719b3f1
yoohyunsun <hyun1909@naver.com>
2019-03-24 16:41:00 +0900

    add kosdaq

--- R/Algo_1/getStock.R
+++ R/Algo_1/getStock.R
@@ -26,8 +26,10 @@ for(j in 1:nrow(kosdaq)){
   })
   hist <- do.call(rbind, hist)
   hist <- cbind(as.Date(hist[,1],"%Y%m%d"),as.data.frame(sapply(hist[,-1],function(x){ as.numeric(x)})))
-  hist <- add_column(hist, c(diff(as.numeric(hist[,2])),0), .after = 2)
-  colnames(hist) <- c("date","last_price","before_price","start_price","high_price","low_price","volume")
+  hist <- add_column(hist, c(diff(as.numeric(hist[,5])),0), .after = 2)
+  #colnames(hist) <- c("date","last_price","before_price","start_price","high_price","low_price","volume")
+  colnames(hist) <- c("date","start_price","before_price","high_price","low_price","last_price","volume")
+  
   hist <- list(hist)
   names(hist) <- code
   histlist <<- append(histlist,hist)
@@ -38,10 +40,12 @@ saveRDS(histlist,file="../kosdaq/kosdaq_1000_20190324.rds")
 
 head(histlist)
 
+
+###°¡°Ýrds
 hist <- lapply(histlist, function(x) {
   x$last_price %>% data.frame(stringsAsFactors = F)
 })
-hist <- do.call(cbind, last_price)
+hist <- do.call(cbind, hist)
 colnames(hist) <-names(histlist)
 hist<-cbind(data=histlist[[1]]$date,hist)
 saveRDS(hist,"../kosdaq/kosdaq_500_20190324_lastprice.rds")
