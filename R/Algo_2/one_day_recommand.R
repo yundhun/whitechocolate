68860968110db9de7ff6e5d5cd2c48a387609d13
PoP <hyun1909@naver.com>
2019-04-17 22:43:19 +0900

    add

--- R/Algo_2/one_day_recommand.R
+++ R/Algo_2/one_day_recommand.R
@@ -18,7 +18,7 @@ rsi_limit <- 60 #RSI 기???
 rsi_trend_days <- 5 #RSI 경향 분석 기간
 
 
-today_date <- "2019-04-12"
+today_date <- "2019-04-17"
 
 trdat = get_train_data(today_date, training_period)
 
@@ -31,14 +31,19 @@ for( clt in clust){
   clt_trdat = trdat[,as.vector(clt)]
   #MSET
   result <- mset_Regress_RF(clt_trdat,predict_period)
+
   mset = result$predict
   mset2 = result$predict2
+  rfresult <- c()
+  k<-0
   mset_recommend_list = mset_filter(mset,predict_period,msetRange)
   
   for(mset_recommend in mset_recommend_list){
     rsi_rule = list('rsi_limit'=rsi_limit,'rsi_trend_days'=rsi_trend_days)
     result = rsi_filter(clt_trdat, mset_recommend, rsi_rule)
     if(result){
+      k<-k+1
+      rfresult <<- c(rfresult,mset_recommend)
       price = tail(clt_trdat[,mset_recommend],1)
       volume = max(1, round(500000/price))
       bid_dat = list('id'=UUIDgenerate(),
@@ -48,10 +53,31 @@ for( clt in clust){
                      'price'=price)
       print(bid_dat)
       wc_plot_2(clt_trdat,mset_recommend,mset2,today_date)
-      #bid_insert_db(bid_dat)
-      #sendEmail(bid_dat)
-      #wc_plot_2(clt_trdat,mset_recommend,mset2, today_date)
-      #bid(bid_dat)
+    }
+  }    
+  
+  result <- mset_Regress(clt_trdat,predict_period)
+  
+  mset = result$predict
+  mset2 = result$predict2
+  lmresult <<- c()
+  
+  mset_recommend_list = mset_filter(mset,predict_period,msetRange)
+  
+  for(mset_recommend in mset_recommend_list){
+    rsi_rule = list('rsi_limit'=rsi_limit,'rsi_trend_days'=rsi_trend_days)
+    result = rsi_filter(clt_trdat, mset_recommend, rsi_rule)
+    if(result){
+      lmresult <- c(lmresult,mset_recommend)
+      price = tail(clt_trdat[,mset_recommend],1)
+      volume = max(1, round(500000/price))
+      bid_dat = list('id'=UUIDgenerate(),
+                     'tr_datetime'=today_date,
+                     'stock_name'=mset_recommend,
+                     'volume'=volume,
+                     'price'=price)
+      print(bid_dat)
+      wc_plot_2(clt_trdat,mset_recommend,mset2,today_date)
     }
   }    
   
